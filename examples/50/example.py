38b9a2948d4ac3b4eb49a854e648a91b9ee45770
Joshua Fogg <joshua.h.fogg@gmail.com>
2024-06-18 15:28:37 +0100

    Updated examples to reflect HiGHS methods

--- examples/50/example.py
+++ examples/50/example.py
@@ -2,12 +2,14 @@
 
 import sys; sys.path.insert(1, '../../')  # HACK pre-module workaround
 import alphargs
+import time
 
 # key problem variables loaded from standard format txt files
 sigma, mubar, omega, n = alphargs.load_problem(
     "A50.txt",
     "EBV50.txt",
-    "S50.txt"
+    "S50.txt",
+    issparse=True
 )
 
 # NOTE this trick of handling sex data is specific to the initial simulation
@@ -19,14 +21,33 @@ dams = range(1, n, 2)
 lam = 0.5
 kap = 1
 
-# computes the standard and robust genetic selection solutions
-w_std, obj_std = alphargs.gurobi_standard_genetics(
-    sigma, mubar, sires, dams, lam, n)
-w_rbs, z_rbs, obj_rbs = alphargs.gurobi_robust_genetics(
+
+# Gurobi using direct optimization
+t0 = time.time()
+w_grb, z_grb, obj_grb = alphargs.gurobi_robust_genetics(
     sigma, mubar, omega, sires, dams, lam, kap, n)
+t1 = time.time()
+print(f"Gurobi took {t1-t0:.5f} seconds (direct)")
 
+# Gurobi using sequential quadratic programming
+t0 = time.time()
+w_grb, z_grb, obj_grb = alphargs.gurobi_robust_genetics_sqp(
+    sigma, mubar, omega, sires, dams, lam, kap, n)
+t1 = time.time()
+print(f"Gurobi took {t1-t0:.5f} seconds (SQP)")
+
+# HiGHS using sequential quadratic programming
+t0 = time.time()
+w_hig, z_hig, obj_hig = alphargs.highs_robust_genetics_sqp(
+    sigma, mubar, omega, sires, dams, lam, kap, n)
+t1 = time.time()
+print(f"HiGHS took  {t1-t0:.5f} seconds (SQP)")
+
+
+print("\nSQP Methods:")
 alphargs.print_compare_solutions(
-    w_std, w_rbs, obj_std, obj_rbs, z2=z_rbs, name1="w_std", name2="w_rbs")
-
+    w_grb, w_hig, obj_grb, obj_hig, z1=z_grb, z2=z_hig,
+    name1="Gurobi", name2="HiGHS ", tol=1e-7
+)
 
 print("\nDone!")
