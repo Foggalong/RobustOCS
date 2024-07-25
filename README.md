235b54dfa83c341dd94a7d6c086752bc96827659
Joshua Fogg <joshua.h.fogg@gmail.com>
2024-07-25 19:30:49 +0100

    Added utility function `solveROCS` and quick-start guide Fixes #17

--- README.md
+++ README.md
@@ -19,9 +19,27 @@ pip install RobustOCS/
 
 Either way, the package depends on Python 3.10+, using [NumPy](https://pypi.org/project/numpy) for linear algebra and [SciPy](https://scipy.org) for sparse matrix objects. As a solver it can either use [Gurobi](https://www.gurobi.com) (commercial) via [gurobipy](https://pypi.org/project/gurobipy) or [HiGHS](https://highs.dev) (free software) via [highspy](https://pypi.org/project/highspy).
 
-## Examples
+## Quick-start
 
-The [GitHub wiki] includes documentation written by which explains the usage and parameters in more detail, alongside some worked examples (the data for which is in [`examples/`](examples/)). This includes a realistic simulated example from [Gregor Gorjanc] and [Ivan Pocrnić].
+Suppose we have a breeding cohort whose relationships are modelled some matrix $\Sigma$, and whose expected breeding values have mean $\bar{\boldsymbol\mu}$ and variance $\Omega$. If these are [saved in files](https://github.com/Foggalong/RobustOCS/wiki/File-Formats), finding the robust optimal contribution selection is as simple as opening Python and running
+
+```python
+import robustocs as rocs
+
+selection, objective, merit, coancestry = rocs.solveROCS(
+    sigma_filename="cohort-relationships.txt",
+    mu_filename="breeding-means.txt",
+    omega_filename="breeding-variances.txt",
+    sex_filename="cohort-sexes.txt",
+    method='robust', lam=0.5, kappa=1
+)
+```
+
+where `selection` will be an array of optimal selections, `objective` is a score of the selection, `merit` the expected genetic merit, and `coancestry` the group co-ancestry.
+
+## Documentation
+
+The [GitHub wiki] includes documentation written by which explains the usage and parameters in more detail, alongside some worked examples using the more granular solver functions (the data for which is in [`examples/`](examples/)). This includes a realistic simulated example from [Gregor Gorjanc] and [Ivan Pocrnić].
 
 [GitHub wiki]: https://github.com/Foggalong/RobustOCS/wiki
 [Gregor Gorjanc]: https://www.ed.ac.uk/profile/gregor-gorjanc
--- robustocs/utils.py
+++ robustocs/utils.py
@@ -10,8 +10,13 @@ import numpy as np          # defines matrix structures
 import numpy.typing as npt  # variable typing definitions for NumPy
 from scipy import sparse    # used for sparse matrix format
 
+# local imports used in solveROCS
+from . import solvers
+from . import loaders
+
 # controls what's imported on `from robustocs.utils import *`
 __all__ = [
+    "solveROCS",
     "expected_genetic_merit",
     "group_coancestry",
     "group_coancestry_fast",
@@ -19,6 +24,233 @@ __all__ = [
     "check_uncertainty_constraint"
 ]
 
+
+# WRAPPER UTILITY
+# The solveROCS combines all of the features in this module into
+# a single interface, handling everything from loading the data
+# from file through to analysing the solution found. While it's
+# convenient for single calls, repeated calls should probably use
+# the more granular functions provided in `solvers`.
+
+def solveROCS(
+    sigma_filename: str,
+    mu_filename: str,
+    sex_filename: str,
+    lam: float,
+    omega_filename: str | None = None,
+    kappa: float | None = None,
+    upper_bound: npt.ArrayLike | float = 1.0,
+    lower_bound: npt.ArrayLike | float = 0.0,
+    method: str = 'standard',
+    solver: str = 'highs',
+    issparse: bool = False,
+    time_limit: float | None = None,
+    max_duality_gap: float | None = None,
+    max_iterations: int = 1000,
+    robust_gap_tol: float = 1e-7,
+    model_output: str = '',
+    debug: bool = False
+):
+    """
+    A convenient wrapper which accesses RobustOCS through a single
+    interface, handling everything from loading the data from file
+    through to analysing the solution found. Best suited for single
+    calls, for more complicated work see the functions in `solvers`.
+
+    Parameters
+    ----------
+    sigma_filename : str
+        Filename for a file which encodes sigma, the relationship matrix
+        for the cohort. This can be either in sparse matrix coordinate
+        format (COO) or pedigree format.
+    mu_filename : str
+        Filename for a file which encodes mu, the expected breeding values
+        of candidates in the cohort. For robust optimization this is mubar,
+        the expected values of the expected returns for candidates in the
+        cohort. The format should be a single value per line.
+    sex_filename: str
+        Filename for a file which encodes sex data, i.e. whether candidates
+        in the cohort are male (sires) or female (dams). The format is a space
+        separated values file with the first column being a label for the
+        and the second being 'M' if the candidate is male and 'F' if female.
+    lam: float,
+        Lambda value to optimize for, which controls the balance between risk
+        and return. Lower values will give riskier portfolios, higher values
+        more conservative ones.
+    omega_filename : str, optional
+        Filename for a file which encodes omega, the covariance matrix for
+        expected returns for candidates in the cohort for selection. This is
+        specific to robust optimization and will be ignored if doing standard
+        (non-robust) optimization. File should be in sparse coordinate format.
+    kappa : float, optional
+        Kappa value to optimize for, which controls how resilient the solution
+        must be to variation in expected values. This is specific to robust
+        optimization and will be ignored if doing standard optimization.
+    upper_bound : ndarray, list, or float, optional
+        Upper bound on how much each candidate can contribute. Can be an array
+        of differing bounds for each candidate, or a float which applies to all
+        candidates. Default value is `1.0`.
+    lower_bound : ndarray, list, or float, optional
+        Lower bound on how much each candidate can contribute. Can be an array
+        of differing bounds for each candidate, or a float which applies to all
+        candidates. Default value is `0.0`.
+    method : str, optional
+        String which specifies whether to carry out 'standard' or 'robust'
+        optimization. Default value is `standard`.
+    solver : str, optional
+        String which specifies whether to use HiGHS or Gurobi as the solver.
+        Value can either be `highs` (default) or `gurobi`.
+    issparse : bool, optional
+        Signifies whether sigma and omega should be loaded primarily as a dense
+        matrix or a sparse one. Default value is `False` (i.e. as dense).
+    time_limit : float, optional
+        Maximum amount of time in seconds to give the solver to find a solution
+        to the problem. Default value is `None`, i.e. no time limit.
+    max_duality_gap : float, optional
+        Maximum allowable duality gap to give the solver when finding a
+        solution to the problem. Default value is `None`, i.e. do not allow
+        any duality gap.
+    max_iterations : int, optional
+        Maximum number of iterations that can be taken in solving the robust
+        problem using SQP. This is specific to robust optimization and will be
+        ignored if doing standard optimization. Default value is `1000`.
+    robust_gap_tol : float, optional
+        Tolerance when checking whether an approximating constraint is active
+        and whether the SQP overall has converged. This is specific to robust
+        optimization and will be ignored if doing standard optimization.
+        Default value is 10^-7.
+    model_output : str, optional
+        Flag which controls whether the solver saves the model file to the
+        working directory. If given, the string is used as the file name,
+        'str.mps', Default value is the empty string, i.e. it doesn't save.
+    debug : bool, optional
+        Flag which controls whether the solver prints its output to terminal.
+        Default value is `False`.
+    """
+
+    # INPUT VALIDATION
+    # ----------------
+
+    # STRINGS
+    # validate key problem variables were provided as strings
+    file_parameters = {
+        "sigma": sigma_filename,
+        "mu": mu_filename,
+        "sexes": sex_filename,
+        "model output": model_output
+    }
+
+    # omega optional to provide, but needs checking for robust problems
+    if omega_filename is not None:
+        file_parameters["omega"] = omega_filename
+
+    # check variable type is a string
+    for name, value in file_parameters.items():
+        if not isinstance(value, str):
+            raise ValueError(f"filename must be given for {name} data")
+
+    # POSITIVE FLOATS
+    # validate variables required to be greater than or equal to zero
+    positive_parameters = {"lambda": lam}
+
+    # kappa optional to provide, but needs checking for robust problems
+    if kappa is not None:
+        positive_parameters["kappa"] = kappa
+
+    # check possible to coerce variable into a float which is positive
+    for name, value in positive_parameters.items():
+        try:
+            if float(lam) < 0:
+                raise ValueError
+        except ValueError:
+            raise ValueError(f"{name} parameter must be a positive number")
+
+    # X IN [0,1]
+    # validate bounds are floats or arrays in interval [0,1]
+    for name, value in {"upper": upper_bound, "lower": lower_bound}.items():
+        if isinstance(value, float):
+            if value < 0 or value > 1:
+                raise ValueError(f"{name} bound must be between 0 and 1")
+        elif isinstance(value, npt.ArrayLike):
+            if any(value < 0 or value > 1):
+                raise ValueError(f"every {name} bound must be between 0 and 1")
+        else:
+            raise ValueError(f"{name} bound must be a number or array")
+
+    # SOLVER & METHOD
+    # validate the method and solver were specified
+    if solver not in ('highs', 'gurobi'):
+        raise ValueError("solver isn't valid, must be 'gurobi' or 'highs'")
+    if method not in ('standard', 'robust'):
+        raise ValueError("method isn't valid, must be 'standard' or 'robust'")
+    # if solving ROCS, validate additional necessary variables are here
+    if (method == 'robust') and kappa is None or omega_filename is None:
+        raise ValueError("robust optimization requires 'kappa' and 'omega'")
+
+    # BOOLEANS
+    # validate sparsity and debug status were specified using booleans
+    for name, value in {"issparse": issparse, "debug": debug}.items():
+        if not isinstance(value, bool):
+            raise ValueError(f"'{name}' must be 'True' or 'False' boolean")
+
+    # STRICTLY POSITIVE FLOATS
+    # validate time_limit and max_duality_gap are strictly positive
+    for name, value in {
+        "time limit": time_limit,
+        "duality gap": max_duality_gap
+    }.items():
+        try:
+            if value is not None and float(value) <= 0:
+                raise ValueError
+        except ValueError:
+            raise ValueError(f"{name} must be a positive number")
+
+    # LOADING & SOLVING
+    # -----------------
+
+    # try to load the variables from file into NumPy
+    sigma, mu, omega, n, sires, dams, _ = loaders.load_problem(
+        sigma_filename, mu_filename, omega_filename, sex_filename,
+        issparse=True
+    )
+
+    if method == 'standard':
+        # Gurobi and HiGHs have the same input variables for standard OCS
+        rocs_solver = getattr(solvers, f"{solver}_standard_genetics")
+        portfolio, objective = rocs_solver(
+            sigma, mu, sires, dams, lam, n, upper_bound, lower_bound,
+            time_limit, max_duality_gap, model_output, debug
+        )
+    elif solver == 'highs':  # and method is robust
+        portfolio, z_value, objective = solvers.highs_robust_genetics(
+            sigma, mu, omega, sires, dams, lam, kappa, n, upper_bound,
+            lower_bound, time_limit, max_duality_gap, max_iterations,
+            robust_gap_tol, model_output, debug
+        )
+    else:  # solver is gurobi, method is robust
+        portfolio, z_value, objective = solvers.gurobi_robust_genetics(
+            sigma, mu, omega, sires, dams, lam, kappa, n, upper_bound,
+            lower_bound, time_limit, max_duality_gap, model_output, debug
+        )
+
+    # CHECKING
+    # --------
+
+    # check the relaxation of the robust term was reasonable
+    if method == 'robust':
+        if not check_uncertainty_constraint(z_value, portfolio, omega,
+                                            robust_gap_tol, debug):
+            raise ValueError("approximation method wasn't within tolerance")
+
+    # quicker to compute useful metrics rather than after the fact
+    EGM = expected_genetic_merit(portfolio, mu)
+    GCA = group_coancestry_fast(mu, lam, objective, portfolio)
+
+    return portfolio, objective, EGM, GCA
+
+
+# ANALYSIS UTILITIES
+# These functions are useful for analysing particular solutions
 
 def expected_genetic_merit(w: npt.ArrayLike, mu: npt.ArrayLike) -> np.floating:
     """
@@ -193,6 +425,9 @@ def print_compare_solutions(
         f"\nMinimum change: {min(portfolio_abs_diff):.{precision}f}"
     )
 
+
+# CHECKING UTILITIES
+# These functions are useful for checking the validity particular solutions
 
 def check_uncertainty_constraint(
     z: float,
